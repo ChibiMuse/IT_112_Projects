76c0ef566713817fc19145602bae77cb52d28f27
ChibiMuse <megan.smith@live.com>
2021-02-28 18:06:39 -0800

    Undid File Delete - Restored Settings.py

--- PythonClub/venv/Scripts/activate.bat
+++ PythonClub/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\megan\Documents\IT_112_Projects\PythonClub\PythonClub\venv
+set VIRTUAL_ENV=C:\Users\megan\Documents\IT_112_Projects\PythonClub\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
