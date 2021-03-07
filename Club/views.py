5dd886b8127af7e4014b9dc244edb57ccbe118a4
ChibiMuse <megan.smith@live.com>
2021-03-07 13:12:39 -0800

    AddsMeetings View

--- Club/views.py
+++ Club/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Meetings, Resource, MeetingMinutes, Event
 
 # Create your views here.
 def index(request):
     return render(request, 'Club/index.html')
+
+def getmeeting(request):
+    meeting_list=Meetings.objects.all()
+    return render(request, 'Club/meetings.html', {'meeting_list' : meeting_list})
